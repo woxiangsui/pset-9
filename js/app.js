be8ad6227354bb247e9a9d4d504dfe74d3118912
Jessica Lin <JLin@ucvts.tec.nj.us>
2020-02-19 12:34:07 -0500

    updated tic tac toe

--- js/app.js
+++ js/app.js
@@ -16,6 +16,8 @@ const winningConditions = [
 let board;
 let turn;
 let win;
+let xScore = 0;
+let oScore = 0;
 
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 
@@ -28,6 +30,7 @@ window.onload = init;
 
 document.getElementById("board").onclick = takeTurn;
 document.getElementById("reset-button").onclick = init;
+document.getElementById("o-button").onclick = oFirst; // tofu *cough *cough fruits
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
 
@@ -35,6 +38,8 @@ function init() {
   board = ["", "", "", "", "", "", "", "", ""];
   turn = "X";
   win = null;
+
+  document.getElementById("o-button").style.display = ""; //shows the button
 
   render();
 }
@@ -74,16 +79,23 @@ function getWinner() {
       board[condition[1]] === board[condition[2]]
     ) {
       winner = board[condition[0]];
+
+      if (winner == "O") { // O points
+          oScore++;
+        document.getElementById("opoints").innerHTML = oScore;
+      } else if (winner == "X") { // X points
+          xScore++;
+        document.getElementById("xpoints").innerHTML = xScore;
+      }
     }
   });
 
   return winner ? winner : board.includes("") ? null : "T";
 }
 
-function recordScore(winner) {
-  if (winner == "X") {
-      xScore++;
-  } else if (winner == "O") {
-      oScore++;
-  }
+function oFirst () { //switches o for x
+  init();
+  turn = "O";
+  document.getElementById("playerTurn").innerHTML = "Turn: O"; // changes turn display
+  document.getElementById("o-button").style.display = "none"; // hides the button
 }
