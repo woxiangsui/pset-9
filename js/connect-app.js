952e65db5d9236025d9da90707d4180463baf296
Jessica Lin <JLin@ucvts.tec.nj.us>
2020-02-27 13:14:13 -0500

    added js code

--- js/connect-app.js
+++ js/connect-app.js
@@ -4,14 +4,42 @@
 
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 
-
+let board;
+let turn;
 
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 
-
+const circles = Array.from(document.querySelectorAll("#board div"));
 
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 
+window.onload = init;
 
+document.getElementById("board").onclick = takeTurn;
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
+
+function init() {
+  board = ["", "", "", "", "", "", "", "", "", "", "", "", "", "",
+           "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+           "", "", "", "", "", "", "", "", "", "", "", "", "", "",];
+  turn = "red";
+  render();
+}
+
+function render() {
+  board.forEach(function(mark, index) {
+    circles[index].textContent = mark;
+  });
+}
+
+function takeTurn(e) {
+  let index = circles.findIndex(function(circle) {
+    return circle === e.target;
+  });
+
+  board[index] = turn;
+  turn = turn === "red" ? "yellow" : "red";
+
+  render();
+}
